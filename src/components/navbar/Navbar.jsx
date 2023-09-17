fbacf989798e257627349e6876bafe58effa53b1
Malinda <malinda@xenosys.lk>
2023-09-18 00:23:56 +0530

    Add Navbar and implement dark and light mode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import styles from "./navbar.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import AuthLinks from "../authLinks/AuthLinks";
+import ThemeToggle from "../themeToggle/ThemeToggle";
 
 const Navbar = () => {
   return (
@@ -12,11 +14,13 @@ const Navbar = () => {
         <Image src={"/tiktok.png"} alt="tiktok" width={24} height={24} />
         <Image src={"/youtube.png"} alt="youtube" width={24} height={24} />
       </div>
-      <div className={styles.logo}>the b.</div>
+      <div className={styles.logo}>the B.</div>
       <div className={styles.links}>
+        <ThemeToggle />
         <Link href="/">Homepage</Link>
         <Link href="/">Contact</Link>
         <Link href="/">About</Link>
+        <AuthLinks />
       </div>
     </div>
   );
